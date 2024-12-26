a737f37d04b27561885fb1ecffff4b62fbb547a9
FerreirAndre <adrecruz15@gmail.com>
2024-12-26 00:32:55 -0300

    adicionando validação para o createUser

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,20 +3,27 @@ package controller
 import (
 	"fmt"
 
-	"github.com/FerreirAndre/go-api/src/configuration/rest_err"
+	"github.com/FerreirAndre/go-api/src/configuration/validation"
 	"github.com/FerreirAndre/go-api/src/controller/model/request"
+	"github.com/FerreirAndre/go-api/src/controller/model/response"
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context){
+func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
-	if err := c.ShouldBindJSON(&userRequest); err != nil{
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("there are some incorrect fields, errors = %s", err),
-			)
-		c.JSON(restErr.Code, restErr)
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		errRest := validation.ValidateUserError(err)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	fmt.Println(response)
 }
